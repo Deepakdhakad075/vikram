72730d5ee31a5d906e524c53c56e663a9f7717f0
Deepakdhakad075 <ddhakad483@gmail.com>
2025-02-11 22:04:31 +0530

    image

---
+++ public/images/home/object/data.js
@@ -0,0 +1,530 @@
+export const slider = {
+    message: "5% Joining bonus on New User IDs. India's most trusted online betting ID Provider. Get DL, MDL, SMDL and Admin at lowest rate."
+  };
+
+export const mainHeading={
+    heading:"Master Panel at Jaw-Dropping Price",
+    paragraph:"Most Reputed, Recommended Online Betting Id Provider. We Are Legal, Easily Accessible On WhatsApp, And Non-Controversial When It Comes To Providing IDs for Online Cricket Betting, Football Betting, Live Casino, Fantasy Betting, Tennis Betting, Online Roulette, Etc. Trusted IDs are available for rent, as well as safe shared betting IDs. Special Offer On Panel, Master Panel, Super Master Panel, Home , MDL, SMDL (Admin). "
+};
+
+export const buttons=[{
+    btnname:"Get Cricket Id Instantly"
+},
+{
+    btnname:"Safe And Secure Transactions"
+},{
+    btnname:"Instant Withdrawals"
+},{
+    btnname:"Welcome Bonus and Customer Support"
+}];
+
+export const offeringHeading={
+heading:"Our Offerings",
+paragraph:"This Is the Right Place for One Who Is Looking for Safe and Secure Betting Platforms in India with –"
+};
+
+export const ourOffers=[{
+    name:"Instant Withdrawals"
+},
+{
+    name:"Football Betting"
+},
+{
+    name:"Fantasy Betting"
+},{
+    name:"Online Roulette"
+},{
+    name:"Online Cricket Betting"
+},{
+    name:"Live Casino"
+},{
+    name:"Tennis Betting"
+},{
+    name:"Poker Ids"
+}
+]
+
+export const details=[{
+    paragraph:"Trusted IDs are available for rent, as well as safe shared betting IDs. Special Offer on Panel, Master Panel, Super Master Panel, Dl, Mdl, SMDL (Admin)."
+},{
+    paragraph:"We Are Just A Chat Away - Click On +916303161135"
+},{
+   paragraph:" India has a population of people who love to play and bet online. Whether you Are A Cricket Fan, Player Or Want To Bet - VikramWorld Is The Best Platform Which Provides *Online Ids* For"
+}]
+
+export const games=[{
+    name:"Online Cricket Betting"
+},
+{
+    name:"Teen Patti"
+},
+{
+    name:"Kabaddi"
+},{
+    name:"PSL betting ID"
+},{
+    name:"OCricket ID"
+},{
+    name:"IPL betting ID"
+},{
+    name:"Tennis"
+},{
+    name:"Poker "
+},{
+    name:"Football"
+},{
+    name:"Online Casino"
+},{
+    name:"Poker Ids"
+},{
+    name:"Betting ID"
+},{
+    name:"Boxing"
+},{
+    name:"Soccer and BPL betting ID"
+},{
+    name:"Wpl Betting ID And More"
+},
+]
+
+export const items=[{
+    img:'',
+    name:"Diamondexch99",
+    paise:"25 paise"
+},
+{
+    img:'',
+    name:"goldenexch99",
+    paise:"25 paise"
+},
+{
+    img:'',
+    name:"d247",
+    paise:"25 paise"
+},{
+    img:'',
+    name:"golden7777",
+    paise:"25 paise"
+},{
+    img:'',
+    name:"lordsexch",
+    paise:"25 paise"
+},{
+    img:'',
+    name:"Diamexch",
+    paise:"25 paise"
+},{
+    img:'',
+    name:"lords365",
+    paise:"25 paise"
+},{
+    img:'',
+    name:"99exch",
+    paise:"25 paise"
+},{
+    img:'',
+    name:"fairsport9 ",
+    paise:"25 paise"
+},{
+    img:'',
+    name:"ceexch",
+    paise:"25 paise"
+},{
+    img:'',
+    name:"the100exch",
+    paise:"25 paise"
+},{
+    img:'',
+    name:"ice777",
+    paise:"25 paise"
+},{
+    img:'',
+    name:	
+    "iceasia9",
+    paise:"25 paise"
+},{
+    img:'',
+    name:"icediamond9",
+    paise:"25 paise"
+},{
+    img:'',
+    name:"iceexch",
+    paise:"25 paise"
+},{
+    img:'',
+    name:	
+    "icecric",
+    paise:"25 paise"
+},{
+    img:'',
+    name:	
+    "the100",
+    paise:"23 paise"
+},{
+    img:'',
+    name:"",
+    paise:"25 paise"
+},{
+    img:'',
+    name:"ice777",
+    paise:"19"
+},{
+    img:'',
+    name:"diamond99",
+    paise:"10 paise"
+},{
+    img:'',
+    name:"Kingexch9",
+    paise:"9 paise"
+},{
+    img:'',
+    name:	
+    "allpaanel",
+    paise:"15 paise"
+},{
+    img:'',
+    name:"",
+    paise:"25 paise"
+},{
+    img:'',
+    name:"tiger365",
+    paise:"16 paise"
+},{
+    img:'',
+    name:"radheexch",
+    paise:"10 paise"
+},{
+    img:'',
+    name:"9wicket",
+    paise:"15 paise"
+},
+{
+    img:'',
+    name:	
+    "saffronexch",
+    paise:"18 paise"
+},
+{
+    img:'',
+    name:"peachexch",
+    paise:"18 paise"
+},
+{
+    img:'',
+    name:	
+    "saffron777",
+    paise:"18 paise"
+},
+{
+    img:'',
+    name:	
+    "armani9",
+    paise:"7 paise"
+},
+{
+    img:'',
+    name:"ice247",
+    paise:"18 paise"
+},
+{
+    img:'',
+    name:	
+    "icebook777",
+    paise:"18 paise"
+},
+{
+    img:'',
+    name:"saffron247",
+    paise:"20 paise"
+},
+{
+    img:'',
+    name:	
+    "6wickets777",
+    paise:"21 paise"
+},
+{
+    img:'',
+    name:	
+    "bossexch9",
+    paise:" 10 paise"
+},
+{
+    img:'',
+    name:	
+    "allpanel777",
+    paise:"17 paise"
+},{
+    img:'',
+    name:	
+    "luckyexch9",
+    paise:"10 paise"
+},{
+    img:'',
+    name:"sapphireexch9",
+    paise:"10 paise"
+},{
+    img:'',
+    name:	
+    "silverbook247",
+    paise:"18 paise"
+},{
+    img:'',
+    name:"silverexch9",
+    paise:" 11paise"
+},{
+    img:'',
+    name:	
+    "winexch99",
+    paise:"11 paise"
+},{
+    img:'',
+    name:"gobook9",
+    paise:"8 paise"
+},{
+    img:'',
+    name:	
+    "gocric9",
+    paise:"10 paise"
+},{
+    img:'',
+    name:	
+    "goid9",
+    paise:"16 paise"
+},{
+    img:'',
+    name:"gomatch9",
+    paise:"10 paise"
+},{
+    img:'',
+    name:	
+    "goasia9",
+    paise:"7 paise"
+},{
+    img:'',
+    name:
+    "goexch247",
+    paise:"16 paise"
+},{
+    img:'',
+    name:"goexch9",
+    paise:"17 paise"
+},{
+    img:'',
+    name:	
+    "exch333",
+    paise:"11 paise"
+},{
+    img:'',
+    name:"abexch9",
+    paise:" 12 paise"
+},{
+    img:'',
+    name:"exch444",
+    paise:"11 paise"
+},{
+    img:'',
+    name:	
+    "playexch9",
+    paise:"8 paise"
+},{
+    img:'',
+    name:	
+    "lotusbook7",
+    paise:" 11 paise"
+},{
+    img:'',
+    name:"icebook9",
+    paise:"18 paise"
+},{
+    img:'',
+    name:	
+    "worldexch9",
+    paise:" paise"
+},{
+    img:'',
+    name:"fairexchange9",
+    paise:"7 paise"
+},{
+    img:'',
+    name:	
+    "exchange666",
+    paise:"7 paise"
+},{
+    img:'',
+    name:	
+    "classicexch99",
+    paise:"17 paise"
+},{
+    img:'',
+    name:"lotusexch247",
+    paise:"14 paise"
+},{
+    img:'',
+    name:"indiaexch",
+    paise:"13 paise"
+},{
+    img:'',
+    name:"lotus247",
+    paise:"18 paise"
+},{
+    img:'',
+    name:	
+    "lotus7book",
+    paise:"27 paise"
+},{
+    img:'',
+    name:	
+    "thelotusbook",
+    paise:"17 paise"
+},{
+    img:'',
+    name:'',
+    paise:" paise"
+},{
+    img:'',
+    name:	
+    "lotusbook247",
+    paise:"17 paise"
+},{
+    img:'',
+    name:	
+    "lotusbook",
+    paise:"18 paise"
+},{
+    img:'',
+    name:	
+    "lotusbook247",
+    paise:"18 paise"
+},{
+    img:'',
+    name:	
+    "matchbox9",
+    paise:"16 paise"
+},{
+    img:'',
+    name:	
+    "matchexch9",
+    paise:"19 paise"
+},{
+    img:'',
+    name:	
+    "tenexch",
+    paise:"22 paise"
+},{
+    img:'',
+    name:'',
+    paise:" paise"
+},{
+    img:'',
+    name:	
+    "vipexch9",
+    paise:"22 paise"
+},{
+    img:'',
+    name:	
+    "kalyanbook9",
+    paise:"17 paise"
+}];
+
+export const easyHeading=[{
+    heading:"Our Executive will Guide You on Chat ...See It's That"
+},{
+    heading:"Easy!!!"
+},{
+    heading:"To Get the Best Online Betting IDs Follow the Steps"
+}]
+
+export const easypara=[{
+    paragraph:"How To Get Master Panel Ids || Super Master Id || Dl Mdl SMDL Admin Id || Agent Id || Panel Id || Available On Rental & Sharing Base|| Dl, MDL, SMDL, Admin Id || Self-Deposit Betting ID"
+},{
+    paragraph:'Follow These Steps.'
+}]
+
+
+export const steps=[{
+    step:"Connect On WhatsApp"
+},{
+    step:"Ask For a Master Panel ID"
+},{
+    step:"The Executive Will Share A Link To the Websites."
+},{
+    step:"Choose And Send Us the Website Name"
+},{
+    step:"Scanner Or Bank Details Will Be Shared"
+},{
+    step:"Minimum coin - 10000"
+},{
+    step:"Master Id Will Be Shared Instantly"
+},{
+    step:"Start Playing. Enjoy Winning The Winning User Id Will Be Credited to Your Account Instantly."
+},]
+
+ export const stepHeading2=[{
+       heading:"How To Get Online Betting Ids.",
+       paragraph:"Cricket Betting Id || Betting Id for Other Games",
+       followsttep:"Follow These Steps."
+ }]
+
+ export const steps2=[{
+    step:"Connect On WhatsApp"
+},{
+    step:"Ask For  ID"
+},{
+    step:" Executive Will Share A Link  Of Sites On Which You Want To Play"
+},{
+    step:"Choose And Send Us the Website Name"
+},{
+    step:"Scanner Or Bank Details Will Be Shared"
+},{
+    step:"Minimum coin - 10000"
+},{
+    step:" Id Will Be Shared Instantly"
+},{
+    step:"Start Playing. Enjoy Winning"
+},{
+    step:" The Winning User Id Will Be Credited to Your Account Instantly."
+}]
+
+
+export const paisa=[{
+    heading:"Paisa ..Paisa .Paisa !!",
+    p:"Haar Gaye Toh Kya -Jeet Gaye Toh Kya If U Win -Money Will Instantly Be Transferred to Your Account.If You Loose - Your Account Will Be Empty, Refill It Again to Play with All Your Skill Sets"
+},{
+    heading:"What About A Security??",
+    p:"It Is the Most Secure, Legal, And Trustworthy Site When It Comes to Master Panel IDs / Super Master IDs / Dl Mdl S Admin Id / Agent Id / Panel Id / Available on Rental & Sharing Base / Dl, Mdl, Smdl, Admin Id / Master Super Master Sharing Rent Self-Deposit Betting Id /Online Betting Id"
+}];
+
+export const imgheading=[{
+    heading:"How To Get A Master Panel ID"
+},{
+    heading:"How To Get A Cricket Betting ID"
+}]
+
+export const btnslearn=[
+    {
+        heading:"Diamond Exchange"
+    },
+    {
+        heading:"Sky Exchange"
+    },
+    {
+        heading:"Lotus Book"
+    },
+    {
+        heading:"World77"
+    },
+]
+
+export const lastpara=[{
+heading:"Disclaimer",
+paragraph:{
+    p1:'There is no uniform national law governing online betting in India. Regulations can vary by state.',
+    p1:'The Public Gambling Act of 1867, though outdated, still forms the basis of much gambling law in India.',
+    p1:'Many states explicitly prohibit online betting, while others have unclear regulations.',
+    p1:'The legality of offshore betting sites operating in India remains a contentious issue.',
+    p1:'There are ongoing debates about regulating and taxing online betting rather than prohibiting it outright',
+    p1:'Participating in illegal gambling activities can lead to legal consequences.'
+   
+}
+}]
